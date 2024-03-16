5470625e412d3381b82e00519860ebe0d8befd02
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-03-16 23:40:22 +0800

    First Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# Name: Diong, Shan Marc C.
+# Section: BSCpE 1-2
+# Program #2: Write a program that will ask you to input your name and your dream job. Print them in a fancy way
+
+# Fancy Word Functions
+blue = "\033[0;34m"
+purple = "\033[0;35m"
+green = "\033[0;32m"
+bold = "\033[1m"
+def fancy_print(name, job):
+    print("\n")
+    print(purple + "=" * 71)
+    print(blue + bold + f"\tHello, {name}! Your dream job is {job}!")
+    print(purple + "=" * 71)
+    print("\n")
+
+# Main Function, Input and Output
+def main():
+    user_name = input(green + bold + "Please enter your name: ")
+    dream_job = input(green + bold + "Please enter your name: ")
+    fancy_print(user_name, dream_job)
+
+main()
+
+#end
